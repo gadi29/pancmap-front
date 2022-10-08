59c5d51f4d1dbf73bb8c714e817522aebf7251a2
Gadiel <gad.azevedo@gmail.com>
2022-10-08 14:50:33 -0300

    style: change font size in login page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -88,7 +88,7 @@ const Container = styled.div`
       border-radius: 7px;
       outline: none;
 
-      font-size: 15px;
+      font-size: 16px;
 
       width: 70%;
       height: 50px;
@@ -103,11 +103,11 @@ const Container = styled.div`
       border: none;
 
       color: #ffffff;
-      font-size: 16px;
-      font-weight: 400;
+      font-size: 18px;
+      font-weight: 600;
 
       width: 20%;
-      height: 40px;
+      height: 50px;
       margin-top: 20px;
       margin-bottom: 35px;
 
@@ -116,7 +116,7 @@ const Container = styled.div`
 
     h3 {
       color: #ffffff;
-      font-size: 13px;
+      font-size: 15px;
       font-weight: 400;
 
       margin-top: 20px;
