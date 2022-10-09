3a1195c1245f07c76f60abdbb104a1fda4b0f5f7
Gadiel <gad.azevedo@gmail.com>
2022-10-09 14:27:04 -0300

    fix: change to back heroku

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const backUrl = "http://localhost:5000";
-//export const backUrl = "https://pancmap.herokuapp.com";
+//export const backUrl = "http://localhost:5000";
+export const backUrl = "https://pancmap.herokuapp.com";
