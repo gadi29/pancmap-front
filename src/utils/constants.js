54f0b39db0d77457ec6aab2aaaf0a9f673074f6a
Gadiel <gad.azevedo@gmail.com>
2022-10-17 14:43:16 -0300

    fix: change api url from heroku to aws

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 //export const backUrl = "http://localhost:5000";
-export const backUrl = "https://pancmap.herokuapp.com";
+export const backUrl = "http://ec2-174-129-61-77.compute-1.amazonaws.com/api";
